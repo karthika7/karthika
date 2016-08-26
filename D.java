cd278d61c4248d52a62cab4201e3635cf9ccdf04
karthika7 <karthikamurugesan13@gmail.com>
2016-08-26 18:25:52 +0530

    Create D.java

---
+++ D.java
@@ -0,0 +1,15 @@
+class D
+{
+public Static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int i=s.nextInt();
+if(i%2==0)
+{
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
+}
